46e7a1e920ba3e8b4a2de382386ec81f988b9dc2
Berker <vojta.janota13@gmail.com>
2023-11-08 12:07:02 +0100

    Update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import NavigationMenu from "./components/navigation";
+import Portfolio from "./pages/portfolio";
+
+function App() {
+  return (
+      < NavigationMenu/>,
+      < Portfolio/>
+  );
+}
+
+export default App;
